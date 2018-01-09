2b0e602fa503114b9688889022d7a8af95c1ec8e
BobWassermann <bob@wassermann.nl>
2018-01-09 22:22:17 +0100

    Optimized js readability

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  if (window.location.href == "http://localhost:9000/") {
-    setTimeout(function () {window.location.href = "/pages/02_inlog/index.html";}, 2000);
-  }
+$(document).ready(() => {
+  switch(window.location.href) {
+    case 'http://localhost:9000/':
+      routeLanding()
+    break;
 
-  if (window.location.href == "http://localhost:9000/pages/06_temperatuur/index.html") {
-        $(".dial").knob({
-          'min':-50,
-          'max':50,
-          'value':75,
-        });
-  }
-});
+    case 'http://localhost:9000/pages/06_temperatuur/index.html':
+      routeTemperature()
+    break;
 
+    default:
+      console.error('Location not found')
+  }
+})
+
+const routeLanding = () => {
+  setTimeout(function () { window.location.href = "/pages/02_inlog/index.html"; }, 2000);
+}
+
+const routeTemperature = () => {
+  $('.dial').knob({
+    'min': -50,
+    'max': 50,
+    'value': 75,
+  })
+}
