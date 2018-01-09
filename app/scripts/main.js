de897a169927f015453ebec1a68fc099f82d89ac
Jérôme Braeken <jeromebraeken@gmail.com>
2018-01-09 21:54:47 +0100

    piewww

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,17 @@
+require("jquery-knob");
+
 document.addEventListener("DOMContentLoaded", function(event) { 
   if (window.location.href == "http://localhost:9000/") {
     setTimeout(function () {window.location.href = "/pages/02_inlog/index.html";}, 2000);
   }
 
   if (window.location.href == "http://localhost:9000/pages/06_temperatuur/index.html") {
+       
+        $(".dial").knob({
+          'min':-50,
+          'max':50,
+          'value':75,
+        });
   }
 });
 
