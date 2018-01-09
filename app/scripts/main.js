12143cd53f5f12134283ad6417e75e12dc933f71
Jérôme Braeken <jeromebraeken@gmail.com>
2018-01-09 21:33:06 +0100

    additions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   if (window.location.href == "http://localhost:9000/") {
-  setTimeout(function () {window.location.href = "/pages/02_inlog/index.html";}, 2000);
+    setTimeout(function () {window.location.href = "/pages/02_inlog/index.html";}, 2000);
+  }
+
+  if (window.location.href == "http://localhost:9000/pages/06_temperatuur/index.html") {
   }
 });
+
